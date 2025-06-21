bbb8702ef79c8a1b6ba6198b7469ad69a0808df3
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2025-06-21 12:37:52 +0800

    Add get article media src function

--- src/utils/getArticleMediaSrc.ts
+++ src/utils/getArticleMediaSrc.ts
@@ -1,3 +1,6 @@
 export default function getArticleMediaSrc(articleId: string, mediaId: string): string {
-    return "";
+    let baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT ?? "";
+    if (!baseUrl.endsWith("/")) baseUrl += "/";
+
+    return `${baseUrl}media/article/${articleId}/${mediaId}`;
 }
